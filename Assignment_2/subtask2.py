2bf364304583fe94c0cea7d4a61ac055f4aa96cf
bhuveetasarohi <142872880+bhuveetasarohi@users.noreply.github.com>
2025-11-29 13:07:59 +0530

    task

--- Assignment_2/subtask2.py
+++ Assignment_2/subtask2.py
@@ -1,16 +1,20 @@
-# subtask2.py
-# Subtask 2: Define System Process Function
-
-import time
 import logging
+import time
+
+logging.basicConfig(
+    filename='process_log.txt',
+    filemode='w',
+    level=logging.INFO,
+    format='%(asctime)s -%(processName)s -%(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 
 def system_process(task_name):
-    """Simulates a system process performing a small task."""
-    logging.info(f"{task_name} started")
+    logging. info(f"{task_name} started")
     time.sleep(2)
     logging.info(f"{task_name} ended")
 
 if __name__ == "__main__":
-    logging.basicConfig(filename='process_log.txt', level=logging.INFO)
-    system_process("DemoProcess")
-    print("System process executed. Check process_log.txt.")
+    print("Running system_process() once ... ")
+    system_process("Test-Process")
+    print("Task completed. Check process_log.txt.")
